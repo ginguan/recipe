10e500ef0d75e54669cce8b752a8ddd76e1bd50d
ginguan <ginalynguan@gmail.com>
2023-01-25 21:15:10 -0800

    adding nav bar

--- src/index.tsx
+++ src/index.tsx
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>    
+    <HashRouter>    
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
