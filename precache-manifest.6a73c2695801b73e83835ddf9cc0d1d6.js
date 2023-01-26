4399d167d8c0c567b0c8906f90b10aed3a5ce5b8
ginguan <ginalynguan@gmail.com>
2023-01-25 18:14:08 -0800

    Deploy React app to GitHub Pages

---
+++ precache-manifest.6a73c2695801b73e83835ddf9cc0d1d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e9e5cbaf55193764703",
+    "url": "/recipe/static/css/main.28192b36.chunk.css"
+  },
+  {
+    "revision": "5e9e5cbaf55193764703",
+    "url": "/recipe/static/js/main.5e9e5cba.chunk.js"
+  },
+  {
+    "revision": "6aa14f7a3799dabaa93f",
+    "url": "/recipe/static/js/1.6aa14f7a.chunk.js"
+  },
+  {
+    "revision": "9c7e7319b0298441b6cc",
+    "url": "/recipe/static/js/2.9c7e7319.chunk.js"
+  },
+  {
+    "revision": "d26a50202e4e24b9b9d4",
+    "url": "/recipe/static/js/runtime~main.d26a5020.js"
+  },
+  {
+    "revision": "d3409f70edfe76166d1913c9e413f977",
+    "url": "/recipe/index.html"
+  }
+];
