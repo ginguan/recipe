b0e1eee87167e9784cfbec33b2251002ec858f4d
ginguan <ginalynguan@gmail.com>
2023-01-26 01:16:19 -0800

    textfield

--- src/components/RandomQuiz.tsx
+++ src/components/RandomQuiz.tsx
@@ -5,27 +5,52 @@ import { useState } from "react";
 import { Drinks } from "../models/Drink";
 import DrinkTable from "./DrinkTable";
 import Header from "./Header";
+import TextField from '@mui/material/TextField';
 
 const RandomQuiz = ({ drinkSet }: { drinkSet: Drinks[] }) => {
 
-    const [drinkSetShuffle, setShuffle] = useState(_.shuffle(drinkSet).slice(0,4))
+    const [drinkSetShuffle, setShuffle] = useState(_.shuffle(drinkSet).slice(0, 4))
     const [showAnswer, setShowAnswer] = useState(false)
     return (
         <div>
-            <Header logo={'QUIZ'}/>
+            <Header logo={'QUIZ'} />
             {drinkSetShuffle.map((member) => (
                 <div style={{ paddingLeft: '45%', paddingTop: '30px', fontWeight: 'bold', fontSize: '20px' }}>
                     {member.name}
                 </div>
             ))}
             <div style={{ paddingLeft: '45%', paddingTop: '30px', fontWeight: 'bold', fontSize: '20px' }}>
-                <Button onClick={() => setShuffle(_.shuffle(drinkSet).slice(0,4))}>Shuffled</Button>
+                <Button onClick={() => setShuffle(_.shuffle(drinkSet).slice(0, 4))}>Shuffled</Button>
             </div>
             <div style={{ paddingLeft: '45%', paddingTop: '30px', fontWeight: 'bold', fontSize: '20px' }}>
                 <Button onClick={() => setShowAnswer(!showAnswer)}>Answers</Button>
             </div>
-            {showAnswer && 
-            <DrinkTable drinkSet={drinkSetShuffle} header={''} isAnswer={true}/>}
+            {showAnswer &&
+                <DrinkTable drinkSet={drinkSetShuffle} header={''} isAnswer={true} />}
+            <div style={{ paddingLeft: '20%' }}>
+                {drinkSetShuffle.map((member) => (
+                    <TextField label={member.name} color="secondary" style={{ padding: '10px' }} />
+                ))}</div>
+                            <div style={{ paddingLeft: '20%', paddingTop: '10px' }}>
+                {drinkSetShuffle.map((member) => (
+                    <TextField
+                        style={{ padding: '10px', maxWidth:'195px' }}
+                        label="method"
+                        multiline
+                        rows={2}
+                    />
+                ))}
+            </div>
+            <div style={{ paddingLeft: '20%', paddingTop: '10px' }}>
+                {drinkSetShuffle.map((member) => (
+                    <TextField
+                        style={{ padding: '10px', maxWidth:'195px' }}
+                        label="ingredient"
+                        multiline
+                        rows={4}
+                    />
+                ))}
+            </div>
         </div>)
 };
 
